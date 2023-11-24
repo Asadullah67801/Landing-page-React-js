756e1b3527249ebfde67265edba8f806a9cf7a4e
asadullah <itsasad9669@gmail.com>
2023-11-24 12:21:26 +0500

    added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Logo11  from '../Assets/Logo11.svg';
+function Navbar() {
+  return (
+    <nav>
+<div className='nav-logo-container'>
+    <img src={Logo11} alt='logo'></img>
+</div>
+
+
+<div className='nav-bar-link-Container'>
+    <a href="#">Home</a>
+    <a href="#">About</a>
+
+
+
+    <a href="#">Testimonial</a>
+    <a href="#">Contact</a>
+
+<a href="#"> <BsCart2 className="navbar-cart-icon" /></a>
+
+<button className="primary-button">Bookings Now</button>
+</div>
+</nav>
+
+  )
+}
+
+export default Navbar
